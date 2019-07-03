fb01aae8f01cfbb9ec736c936313bdc811e10ac9
coded_ <selasikudolo@cmail.carleton.ca>
2019-07-02 23:10:08 -0400

    added code for deployment to github pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Center from 'react-center';
-//import Button from 'react-bootstrap/Button'
-//import ButtonNice from 'react-button-nice';
+
 
 function Square(props) {
     return (
