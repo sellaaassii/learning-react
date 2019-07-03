75cf21e12e3f05f1b5712cc4758ebcd4ab1cd486
coded_ <selasikudolo@cmail.carleton.ca>
2019-07-02 23:13:13 -0400

    Updates

---
+++ precache-manifest.dc620bcafec24dc011d0f412e12f6278.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317d95219dcbf417a045dc9b0fa8a10c",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "fcad38edea7c475cf26d",
+    "url": "/learning-react/static/css/main.bc67e473.chunk.css"
+  },
+  {
+    "revision": "fd169d37287c10524680",
+    "url": "/learning-react/static/js/2.447b3afc.chunk.js"
+  },
+  {
+    "revision": "fcad38edea7c475cf26d",
+    "url": "/learning-react/static/js/main.201798a7.chunk.js"
+  },
+  {
+    "revision": "e639320f9f284d9d0fff",
+    "url": "/learning-react/static/js/runtime~main.55738a4d.js"
+  }
+]);
