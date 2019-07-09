6d5f92812183b043d23bbfcacf1edefe590fac12
coded_ <selasikudolo@cmail.carleton.ca>
2019-07-09 00:35:31 -0400

    Updates

---
+++ precache-manifest.c3565b90ea3a306b072791fec04afe31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a38387396f03a42555b1f9aff4151b",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "f13af975a9b551616c76",
+    "url": "/learning-react/static/css/main.dd8ce4a7.chunk.css"
+  },
+  {
+    "revision": "613a554ab229d35193c5",
+    "url": "/learning-react/static/js/2.5404b820.chunk.js"
+  },
+  {
+    "revision": "f13af975a9b551616c76",
+    "url": "/learning-react/static/js/main.0092bfdd.chunk.js"
+  },
+  {
+    "revision": "e639320f9f284d9d0fff",
+    "url": "/learning-react/static/js/runtime~main.55738a4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/learning-react/precache-manifest.9db7e84266f7875c8279db8931d08ed5.js"
+  "/learning-react/precache-manifest.c3565b90ea3a306b072791fec04afe31.js"
 );
 
 self.addEventListener('message', (event) => {
